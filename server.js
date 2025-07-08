4429353a53a2b123278ceb5f6767ddf348119c64
rustamova148 <79011363+rustamova148@users.noreply.github.com>
2025-07-08 17:12:57 +0400

    server.js fix

--- server.js
+++ server.js
@@ -1,34 +1,15 @@
 import jsonServer from 'json-server';
-// import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const server = jsonServer.create(); 
 
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3001;
 
-// Middlewares
 server.use(middlewares);
+server.use("/api", router);
 
-// Static frontend build göstərmək üçün
-server.use(express.static(path.join(__dirname, 'dist')));
-
-// API endpoint
-server.use(router);
-
-// Frontend router dəstəyi üçün (SPA)
-server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-// Serveri başladırıq
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`JSON Server is running on port ${PORT}`);
 });
-
-
