dc893ae028e22cb15ebf5d0410a13ced31b58d5e
rustamova148 <79011363+rustamova148@users.noreply.github.com>
2025-07-08 16:54:46 +0400

    change for deploy

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
 import jsonServer from 'json-server';
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import express from "express";
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const server = express();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3001;
 
+// Middlewares
 server.use(middlewares);
-server.use(express.static(path.join(__dirname, "dist")));
 
-server.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+// Static frontend build göstərmək üçün
+server.use(express.static(path.join(__dirname, 'dist')));
+
+// API endpoint
+server.use('/api', router);
+
+// Frontend router dəstəyi üçün (SPA)
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-server.use("/api", router);
-
+// Serveri başladırıq
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
