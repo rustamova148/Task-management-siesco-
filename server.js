84bb2beeb7cd7b8d80146979f05b8187d7afcc19
rustamova148 <79011363+rustamova148@users.noreply.github.com>
2025-07-08 17:18:35 +0400

    change in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
-server.use("/api", router);
+server.use(router);
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
