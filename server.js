4a820f516a4c55aa43a16394263d78d430c718c2
rustamova148 <79011363+rustamova148@users.noreply.github.com>
2025-07-08 17:06:25 +0400

    last

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import jsonServer from 'json-server';
-import express from 'express';
+// import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
